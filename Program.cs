39dbe976a5504df0eb282445fb16c7c6f389b765
emper1or <emper1or38@yandex.ru>
2024-03-08 17:35:37 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_3
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+
+        }
+    }
+}
